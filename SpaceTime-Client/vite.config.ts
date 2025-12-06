97419aec5218747cb20ccf883dfadae57ad1abe4
Space Time <Zeus6_6@163.com>
2025-12-06 14:18:23 +0800

    0.1.13 更新

--- SpaceTime-Client/vite.config.ts
+++ SpaceTime-Client/vite.config.ts
@@ -1,14 +1,9 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy'
-import { env } from 'process';
 import vue from '@vitejs/plugin-vue';
-import fs from 'fs';
 
 export default defineConfig({
-  plugins: [
-    vue()
-  ],
+  plugins: [vue()],
   css: {
     preprocessorOptions: {
       less: {
@@ -20,9 +15,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  server: {
-    port: 5173,
-    host: true
   }
 })
