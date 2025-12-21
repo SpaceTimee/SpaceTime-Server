0c853444ee0d4e8c46c7d5e06bb3f3fab4fd90ac
Space Time <Zeus6_6@163.com>
2025-12-21 20:27:17 +0800

    0.2.2 更新

--- SpaceTime-Server/Program.cs
+++ SpaceTime-Server/Program.cs
@@ -6,23 +6,23 @@ using System;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-builder.AddServiceDefaults();
-builder.Services.AddResponseCaching();
-builder.Services.AddResponseCompression(static options => options.EnableForHttps = true);
-builder.Services.AddRequestTimeouts(static options => options.DefaultPolicy = new() { Timeout = TimeSpan.FromMinutes(10) });
-builder.Services.AddDirectoryBrowser();
-builder.Services.AddOpenApi();
+builder.AddServiceDefaults().
+    Services.AddResponseCaching().
+    AddResponseCompression(static options => options.EnableForHttps = true).
+    AddRequestTimeouts(static options => options.DefaultPolicy = new() { Timeout = TimeSpan.FromMinutes(10) }).
+    AddDirectoryBrowser().
+    AddOpenApi();
 
 WebApplication app = builder.Build();
 
-app.UseHttpsRedirection();
-app.UseResponseCaching();
-app.UseResponseCompression();
-app.UseRequestTimeouts();
-app.UseFileServer(new FileServerOptions { EnableDirectoryBrowsing = true });
+app.UseHttpsRedirection().
+    UseResponseCaching().
+    UseResponseCompression().
+    UseRequestTimeouts().
+    UseFileServer(new FileServerOptions { EnableDirectoryBrowsing = true });
+
 app.MapStaticAssets();
 app.MapDefaultEndpoints();
 app.MapOpenApi();
 app.MapScalarApiReference("/api/");
-
 app.Run();
