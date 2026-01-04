60f3af18692e7d98b8e675a3913c6c4623827e5d
Space Time <Zeus6_6@163.com>
2026-01-04 22:14:14 +0800

    0.3.6 更新

--- SpaceTime-Server/Utilities/FileIconResolver.cs
+++ SpaceTime-Server/Utilities/FileIconResolver.cs
@@ -6,26 +6,34 @@ internal static class FileIconResolver
 {
     internal static string Resolve(string mime) => string.IsNullOrEmpty(mime) ? "icon-folder" : mime switch
     {
-        _ when mime.StartsWith("image/", StringComparison.Ordinal) => "icon-image",
-        _ when mime.StartsWith("video/", StringComparison.Ordinal) || mime is "application/mp4" => "icon-video",
+        _ when mime.StartsWith("image/", StringComparison.Ordinal)
+            || mime is "application/postscript" => "icon-image",
+
+        _ when mime.StartsWith("video/", StringComparison.Ordinal)
+            || mime is "application/mp4" or "application/vnd.rn-realmedia" or "application/vnd.rn-realmedia-vbr"
+            or "application/x-shockwave-flash" or "model/vnd.mts" => "icon-video",
+
         _ when mime.StartsWith("audio/", StringComparison.Ordinal) => "icon-audio",
-        _ when mime is "application/zip" or "application/gzip" or "application/x-gzip"
-            or "application/x-tar" or "application/x-bzip2" or "application/x-xz"
-            or "application/x-7z-compressed" or "application/x-rar-compressed" or "application/vnd.rar"
+
+        _ when mime is "application/zip" or "application/gzip"
+            or "application/x-tar" or "application/x-bzip" or "application/x-bzip2" or "application/x-xz"
+            or "application/x-7z-compressed" or "application/vnd.rar"
             or "application/java-archive" or "application/vnd.android.package-archive"
-            or "application/x-apple-diskimage" or "application/x-iso9660-image" => "icon-archive",
-        _ when mime is "text/html" or "text/css" or "text/xml"
-            or "text/javascript" or "text/ecmascript" or "text/typescript"
-            or "text/x-python" or "text/x-java" or "text/x-c" or "text/x-csrc" or "text/x-c++" or "text/x-c++src"
-            or "text/x-csharp" or "text/x-go" or "text/x-rust" or "text/x-php" or "text/x-ruby"
-            or "text/x-swift" or "text/x-kotlin" or "text/x-scala" or "text/x-perl" or "text/x-r"
-            or "text/x-lua" or "text/x-dart" or "text/x-sh" or "text/x-shellscript" or "text/x-powershell"
-            or "application/javascript" or "application/ecmascript" or "application/typescript"
-            or "application/json" or "application/xml" or "application/yaml" or "application/toml"
+            or "application/vnd.ms-cab-compressed" or "application/x-redhat-package-manager"
+            or "application/appx" or "application/msix"
+            or "application/x-debian-package" or "application/x-freearc" => "icon-archive",
+
+        _ when mime is "text/html" or "text/css"
+            or "text/javascript" or "text/jsx" or "text/coffeescript" or "text/x-java-source" or "text/x-asm" or "text/yaml"
+            or "application/javascript" or "application/ecmascript"
+            or "application/json" or "application/ld+json" or "application/xml" or "application/toml"
             or "application/sql" or "application/wasm"
-            or "application/x-python" or "application/x-perl" or "application/x-ruby"
-            or "application/x-httpd-php" or "application/x-sh" or "application/x-powershell" => "icon-code",
-        _ when mime.StartsWith("text/", StringComparison.Ordinal) => "icon-text",
+            or "application/x-perl" or "application/x-httpd-php" or "application/x-sh" or "application/x-csh"
+            or "application/java-vm" or "application/vnd.dart" or "application/x-msdownload" => "icon-code",
+
+        _ when mime.StartsWith("text/", StringComparison.Ordinal)
+            || mime is "application/rtf" or "application/x-subrip" => "icon-text",
+
         _ => "icon-file"
     };
 }
